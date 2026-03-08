21e274fb572d2ac09e4142e70cbf5598cbe6851e
Mirlinnn <gayday10@inbox.ru>
2026-03-08 23:04:39 +0900

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,21 @@
+#Токен
+
 BOT_TOKEN = "8563322655:AAF2GypxWciOFt1SKu4Zdnw0dgqSPfYWyOA"
+#Юкасса
+
 YOOKASSA_PAYMENT_TOKEN = "381764678:TEST:170098"
 YOOKASSA_SHOP_ID = "1295765"
 YOOKASSA_SECRET_KEY = "test_W6lncrq6m_3IfpjMqCBLPUzpqcVm6BiHcKAJZ_y-yos"
 YOOKASSA_RETURN_URL = "https://t.me/Winixibot"  # куда вернуться после оплаты
+
+#Крипта(касса(Heleket))
+HELEKET_API_KEY = "ваш_api_ключ"
+HELEKET_API_URL = "https://api.heleket.com/v1"  # уточните в документации
+HELEKET_RETURN_URL = "https://t.me/your_bot_username"  # куда вернуться после оплаты
+
+#Админы
 ADMINS = [6384359588]  # ID админов
 
+#Хс, нужно ли
 CARD_DETAILS = "💳 2200702011077075"
 CRYPTO_DETAILS = "💰 TON UQDIJouCK1D9eBJPbDAUtlUaLNJNOhADHtVpx1dK4qziqHGm"
