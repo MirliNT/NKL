7e5388ba571ff4087bba77d3372e41e18de76145
Mirlinnn <gayday10@inbox.ru>
2026-03-09 21:51:56 +0900

    Update config.py

--- config.py
+++ config.py
@@ -9,9 +9,10 @@ YOOKASSA_SECRET_KEY = "test_W6lncrq6m_3IfpjMqCBLPUzpqcVm6BiHcKAJZ_y-yos"
 YOOKASSA_RETURN_URL = "https://t.me/Winixibot"  # куда вернуться после оплаты
 
 #Крипта(касса(Heleket))
-HELEKET_API_KEY = "0c2dceb5-6400-4678-ad6a-52922d3d4ea4"
-HELEKET_API_URL = "https://api.heleket.com/v1"  # уточните в документации
-HELEKET_RETURN_URL = "https://t.me/Winixibot"  # куда вернуться после оплаты
+HELEKET_MERCHANT_ID = "0c2dceb5-6400-4678-ad6a-52922d3d4ea4"          # из личного кабинета Heleket
+HELEKET_API_KEY = "TWBK2a95QTvuXpgl6LOCQfbQ60VftpMoNueUx4M7TDENZdWWu4bsUYS3ymv4dL94N6WFj4pI8wcNJix1tsYV9bFx9XAckVQEyO1SI8RiUk5Y8sFrmfs1vpNeip4giL7G"             # из личного кабинета
+HELEKET_API_URL = "https://api.heleket.com/v1"
+HELEKET_RETURN_URL = "https://t.me/Winixibot"  # ссылка на бота для возврата после оплаты
 
 #Админы
 ADMINS = [6384359588]  # ID админов
