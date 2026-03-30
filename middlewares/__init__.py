fc312dad74ea40472f14d96113eb16ec7e689101
Mirlinnn <gayday10@inbox.ru>
2026-03-31 00:52:49 +0900

    Update __init__.py

--- middlewares/__init__.py
+++ middlewares/__init__.py
@@ -1,1 +1,9 @@
-.
+from .ban_check import BanCheckMiddleware
+from .bot_status import BotStatusMiddleware
+from .logging import LoggingMiddleware
+
+__all__ = [
+    'BanCheckMiddleware',
+    'BotStatusMiddleware',
+    'LoggingMiddleware',
+]
