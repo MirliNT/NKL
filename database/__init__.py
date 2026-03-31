58efd105b30729380afd67843921d41248850537
Mirlinnn <gayday10@inbox.ru>
2026-03-31 13:54:44 +0900

    Update __init__.py

--- database/__init__.py
+++ database/__init__.py
@@ -67,9 +67,68 @@ from .admins import (
     get_all_admins
 )
 
+from .bot_state import (
+    is_bot_active,
+    get_bot_status,
+    set_bot_active
+)
+
 from .settings_db import (
     create_settings_table,
     get_setting,
     set_setting,
     get_all_settings
 )
+
+__all__ = [
+    'init_db',
+    'get_connection',
+    'execute',
+    'fetchone',
+    'fetchall',
+    'execute_many',
+    'transaction',
+    'add_user',
+    'get_balance',
+    'update_balance',
+    'set_balance',
+    'is_banned',
+    'get_ban_info',
+    'ban_user',
+    'unban_user',
+    'has_accepted_terms',
+    'accept_terms',
+    'get_all_users',
+    'create_order',
+    'get_order',
+    'update_order_status',
+    'update_order_payment_id',
+    'update_order_payment_method',
+    'get_orders_by_status',
+    'add_service',
+    'get_service',
+    'get_services_by_platform',
+    'get_services_by_category',
+    'get_services_by_subcategory',
+    'update_service_price',
+    'update_service_speed',
+    'update_service_description',
+    'update_all_prices',
+    'add_promocode',
+    'get_promocode',
+    'use_promocode',
+    'add_transaction',
+    'get_transactions',
+    'get_all_transactions',
+    'add_admin',
+    'remove_admin',
+    'is_admin',
+    'get_all_admins',
+    'is_bot_active',
+    'get_bot_status',
+    'set_bot_active',
+    'create_settings_table',
+    'get_setting',
+    'set_setting',
+    'get_all_settings',
+]
